c3a03dd00a34ec59d04dcea4d3830b2192d6a573
diegorkm <diegoaperaltar@gmail.com>
2025-09-25 21:20:32 -0300

    Agregado clases faltantes, aun falta completar los metodos

--- dungeon_generator.py
+++ dungeon_generator.py
@@ -3,20 +3,21 @@ from abc import ABC, abstractmethod
 
 
 class Habitacion:
-    def __init__(self, id: int, x: int, y: int, inicial: bool, contenido: ContenidoHabitacion | None, conexiones: dict[str, Habitacion], visitado: bool):
+    def __init__(self, id: int, x: int, y: int, inicial: bool, contenido: ContenidoHabitacion | None, conexiones: dict[str, Habitacion], visitada: bool):
         self.id = id
         self.x = x
         self.y = y
         self.inicial = inicial
         self.contenido = contenido
         self.conexiones = conexiones
-        self.visitado = visitado
+        self.visitada = visitada
 
 class Mapa: # Implementar metodos
     def __init__(self, ancho: int, alto: int, habitaciones: dict[tuple[int, int], Habitacion], habitacion_inicial: Habitacion):
         self.ancho = ancho
         self.alto = alto
         self.habitaciones = habitaciones
+        self.habitacion_inicial = habitacion_inicial
 
     def generar_estructura(self, n_habitaciones: int):
         pass
@@ -53,23 +54,84 @@ class Explorador: # Implementar metodos
         pass
 
     @property
-    def  esta_vivo(self) -> bool:
+    def esta_vivo(self) -> bool:
         pass
 
 class ContenidoHabitacion(ABC):
+    @property
     @abstractmethod
-    def __init__(self):
+    def descripcion(self) -> str:
         pass
 
     @property
-    def descripcion() -> str:
+    @abstractmethod
+    def tipo(self) -> str:
+        pass
+
+    @abstractmethod
+    def interactuar() -> str:
+        pass
+
+class Tesoro(ContenidoHabitacion):
+    def __init__(self, recompensa: Objeto):
+        self.recompensa = recompensa
+        
+    @property
+    def descripcion(self) -> str:
         pass
 
     @property
-    def tipo() -> str:
+    def tipo(self) -> str:
         pass
 
-    def interactuar(self) -> str:
+    def interactuar() -> str:
+        pass
+
+class Monstruo(ContenidoHabitacion):
+    def __init__(self, nombre: str, vida: int, ataque: int):
+        self.nombre = nombre
+        self.vida = vida
+        self.ataque = ataque
+
+    @property
+    def descripcion(self) -> str:
+        pass
+
+    @property
+    def tipo(self) -> str:
+        pass
+
+
+    def interactuar() -> str:
+        pass
+
+class Jefe(Monstruo):
+    def __init__(self, nombre: str, vida: int, ataque: int, recompensa_especial: Objeto):
+        super().__init__(nombre, vida, ataque)
+        self.recompensa_especial = recompensa_especial
+
+    @property
+    def tipo(self) -> str:
+        pass
+
+    def interactuar() -> str:
+        pass
+
+class Evento(ContenidoHabitacion):
+    def __init__(self, nombre: str, descripcion: str, efecto):
+        self.nombre = nombre
+        self._descripcion = descripcion
+        self.efecto = efecto
+
+    @property
+    def descripcion(self) -> str:
+        return self._descripcion
+    
+    @property
+    def tipo(self) -> str:
+        pass
+    
+    def interactuar() -> str:
         pass
 
 class Visualizador:
@@ -88,6 +150,5 @@ class Visualizador:
     def  mostrar_estado_explorador(self, explorador: Explorador):
         pass
 
-class Evento:
-    pass
+
 
